0bdc09e4855d1c3c3437e578c7faaaf9ad930b02
geddadalakshmanrao <lakshmangeddada1994@gmail.com>
2023-03-06 15:58:23 +0530

    updated

--- learn-terraform/sample01/create-ec2.tf
+++ learn-terraform/sample01/create-ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ec2" {
   ami = "ami-0a017d8ceb274537d"
   instance_type = "t3.micro"
-  vpc_security_groups_ids = [ "sg-0615d2d6b16a84caa" ]
+  vpc_security_group_ids = [ "sg-0615d2d6b16a84caa" ]
   tags = {
     Name: "test"
   }
