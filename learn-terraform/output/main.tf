aad7666980795a1d7162a8e132983508a9a64bac
geddadalakshmanrao <lakshmangeddada1994@gmail.com>
2023-03-06 16:09:52 +0530

    updated

--- learn-terraform/output/main.tf
+++ learn-terraform/output/main.tf
@@ -5,3 +5,16 @@ output "sample" {
 output "sample1" {
   value = "welcome home"
 }
+
+resource "aws_instance" "ec2" {
+  ami = "ami-0a017d8ceb274537d"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [ "sg-0615d2d6b16a84caa" ]
+  tags = {
+    Name: "test"
+  }
+}
+
+output "security_id" {
+  value = vpc_security_group_ids
+}
