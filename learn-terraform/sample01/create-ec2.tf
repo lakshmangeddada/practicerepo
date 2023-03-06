541fbf7054ad4ac41c6e974603a30276fc440d1a
geddadalakshmanrao <lakshmangeddada1994@gmail.com>
2023-03-06 15:52:43 +0530

    updated

--- learn-terraform/sample01/create-ec2.tf
+++ learn-terraform/sample01/create-ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ec2" {
   ami = "ami-0a017d8ceb274537d"
   instance_type = "t3.micro"
-  security_groups = ["sg-0615d2d6b16a84caa"]
+  security_groups = [ "sg-0615d2d6b16a84caa" ]
   tags = {
     name: "test"
   }
