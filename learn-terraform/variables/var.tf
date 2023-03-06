2e7c3d626a7a93d60a33866f43659f35abca1f08
geddadalakshmanrao <lakshmangeddada1994@gmail.com>
2023-03-06 19:43:37 +0530

    updated

--- learn-terraform/variables/var.tf
+++ learn-terraform/variables/var.tf
@@ -12,6 +12,28 @@ output "sample1" {
   value = var.sample1
 }
 
+variable "sample2" {
+  default = [
+    "welcome to session",
+    100
+  ]
+}
+
+variable "sample3" {
+  default = {
+    string = lakshman
+    boolean = true
+
+  }
+}
+
+output "sample2" {
+  value = "first variable in the list - ${var.sample2[0]}"
+}
+
+output "sample3" {
+  value = "second variable is  - ${var.sample3 ["boolean"]}"
+}
 //------------------------
 
 /*
