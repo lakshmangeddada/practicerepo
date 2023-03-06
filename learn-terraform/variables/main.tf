eb3b1327d89958f72e449d9f833758f51c5a2f16
geddadalakshmanrao <lakshmangeddada1994@gmail.com>
2023-03-06 17:06:06 +0530

    updated

--- learn-terraform/variables/main.tf
+++ learn-terraform/variables/main.tf
@@ -1,7 +1,19 @@
-output "instance_type" {
-  value = "${var.instance_type}"
+variable "sample" {
+  default = "welcome"
 }
 
+output "sample" {
+  value = var.sample
+}
+
+variable "sample1" {}
+
+output "sample1" {
+  value = var.sample1
+}
+//------------------------
+
+/*
 variable "instance_type" {
   default = "t3.micro"
 }
@@ -15,4 +27,8 @@ resource "aws_instance" "ec2" {
     Name: "test"
   }
 }
+
+output "instance_type" {
+  value = "${var.instance_type}"
+}*/
 
