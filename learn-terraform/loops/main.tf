8c08e3c32344b81bbfe181463358158dad1e0ff8
geddadalakshmanrao <lakshmangeddada1994@gmail.com>
2023-03-07 12:07:47 +0530

    updated

---
+++ learn-terraform/loops/main.tf
@@ -0,0 +1,32 @@
+variable "d1" {
+  default = 5
+}
+
+resource "null_resource" "nothing" {
+  count = var.d1
+}
+
+variable "d2" {
+  default = [ "apple", "banana" ]
+}
+
+resource "null_resource" "something" {
+  count = length(var.d2)
+}
+
+variable "d3" {
+  default = {
+    apple = {
+      name = "apple"
+      count = 3
+    }
+    banana = {
+      name = "banana"
+      count = 2
+    }
+  }
+}
+
+resource "null_resource" "anything" {
+  for_each = var.d3
+}
