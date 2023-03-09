bb30d38b9ab15a134c4207bf0a2d1fe7fc2a1d4d
geddadalakshmanrao <lakshmangeddada1994@gmail.com>
2023-03-09 12:30:45 +0530

    updated

---
+++ learn-terraform/functions/main.tf
@@ -0,0 +1,16 @@
+variable "instances" {
+  default = {
+    cart = {
+      name = "cart"
+      type = "t3.micro"
+    }
+    user = {
+      name = "user"
+      type = "t3.small"
+    }
+  }
+}
+
+output "instances" {
+  value = var.instances
+}
